fe49a2738158b475c80de8b326afdda575765de6
Glitch (hello-webpage) <none>
2021-01-28 13:29:43 +0000

    👍🏬 Checkpoint ./index.html:27800240/356 ./style.css:27800240/200 ./script.js:27800240/102

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+// prints "Hello from the Web App Dev 1 lab!" in the browser's dev tools console
+console.log('Hello from the Web App Dev 1 lab!');
