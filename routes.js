4651cd0c851d0e0abfae82498423a451a02c10d6
Glitch (hello-webpage) <none>
2021-02-11 20:43:04 +0000

    🤑🐷 Checkpoint ./routes.js:27800240/498 ./server.js:27800240/252 ./views/partials/menu.hbs:27800240/57 ./controllers/about.js:27800240/781

---
+++ routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// import express and initialise router
+const express = require('express');
+const router = express.Router();
+
+// import controllers
+const start = require('./controllers/start.js');
+const dashboard = require('./controllers/dashboard.js');
+const about = require('./controllers/about.js');
+
+// connect routes to controllers
+router.get('/', start.index);
+router.get('/dashboard', dashboard.index);
+
+// export router module
+module.exports = router;
