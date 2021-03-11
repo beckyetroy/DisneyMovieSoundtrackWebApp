00ec64eabd9e3a451bb222dae87f867573b560af
Glitch (week6lab5) <none>
2021-03-11 13:56:34 +0000

    🐧🐵 Checkpoint ./views/partials/addsong.hbs:27800240/520 ./controllers/playlist.js:27800240/73 ./controllers/dashboard.js:27800240/39 ./views/partials/addplaylist.hbs:27800240/281

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -36,6 +36,7 @@ const dashboard = {
     const newPlayList = {
       id: uuid(),
       title: request.body.title,
+      duration: request.body.duration,
       songs: [],
     };
     playlistStore.addPlaylist(newPlayList);
--- controllers/playlist.js
+++ controllers/playlist.js
@@ -30,6 +30,8 @@ const playlist = {
       id: uuid(),
       title: request.body.title,
       artist: request.body.artist,
+      genre: request.body.genre,
+      duration: request.body.duration
     };
     playlistStore.addSong(playlistId, newSong);
     response.redirect('/playlist/' + playlistId);
