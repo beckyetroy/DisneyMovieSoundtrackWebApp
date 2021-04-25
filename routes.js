742e7c8f4493a7c9b45f54a3c9227913a27f158a
Glitch (week6lab5) <none>
2021-04-25 13:24:40 +0000

    🚨🚶 Checkpoint ./public/style.css:27800240/152 ./controllers/accounts.js:27800240/1779 ./routes.js:27800240/312

--- routes.js
+++ routes.js
@@ -9,15 +9,22 @@ const start = require('./controllers/start.js');
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
 const movie = require('./controllers/movie.js');
+const accounts = require ('./controllers/accounts.js');
 
 // connect routes to controllers
-router.get('/', start.index);
+router.get('/start', start.index);
 router.get('/dashboard', dashboard.index);
 router.get('/about', about.index);
 router.get('/movie/:id', movie.index);
 router.get('/movie/:id/deleteTrack/:trackid', movie.deleteTrack);
 router.get('/dashboard/:id/deleteMovie', dashboard.deleteMovie);
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
 
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
 router.post('/movie/:id/addtrack', movie.addTrack);
 router.post('/dashboard/addmovie', dashboard.addMovie);
 
