5c834f0a76c4d2e27ea45138b1240dd009c9a20a
Glitch (week6lab5) <none>
2021-04-26 11:04:20 +0000

    🚏⏳ Checkpoint ./views/login.hbs:27800240/2042 ./public/script.js:27800240/1409 ./public/style.css:27800240/236

--- public/script.js
+++ public/script.js
@@ -136,14 +136,3 @@ const showHideAbout = _ =>
     doubleDown.className = "angle double up icon";
   }
 }
-
-const showHideLogInError = _ =>
-{
-  let message = document.getElementById("logInError");
-  if (message.style.display === "block") {
-    message.style.display === "none";
-  }
-  else {
-    message.style.display === "block";
-  }
-}
