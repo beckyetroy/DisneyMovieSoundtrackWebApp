516e35bbbc18ae1c3aaf73fdb89a33a0dcc6c170
Glitch (week6lab5) <none>
2021-04-28 18:13:22 +0000

    🚕🖐 Checkpoint ./controllers/editmovie.js:27800240/2568 ./routes.js:27800240/28 ./views/partials/listtracks.hbs:27800240/9 ./views/partials/edittracks.hbs:27800240/741

---
+++ controllers/editmovie.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const logger = require('../utils/logger');
+const movieStore = require('../models/movie-store');
+const uuid = require('uuid');
+const accounts = require ('./accounts.js');
+
+const editmovie = {
+  index(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);  
+    const movieId = request.params.id;
+    logger.debug('Movie id = ' + movieId);
+    if (loggedInUser) {
+    const viewData = {
+      title: movieStore.getMovie(movieId).title,
+      movie: movieStore.getMovie(movieId),
+      fullname: loggedInUser.firstName + ' ' + loggedInUser.lastName,
+      picture: loggedInUser.picture,
+    };
+    response.render('editmovie', viewData);
+    }
+    else response.redirect('/');
+},
+  
+   updateTrack(request, response) {
+    const movieId = request.params.id;
+    const trackId = request.params.trackId;
+    logger.debug("updating track " + trackId);
+    const updatedTrack = {
+      title: request.body.title,
+      singer: request.body.singer,
+      genre: request.body.genre,
+      youtube: request.body.youtube,
+    };
+    movieStore.editTrack(movieId, trackId, updatedTrack);
+    response.redirect('/movie/' + movieId);
+  },
+};
+
+module.exports = editmovie;
